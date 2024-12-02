63136b9714cf7f4b66b4cfdc144d73639f3bb367
Anna Karutina <anna.karutina@khk.ee>
2024-12-02 09:00:47 +0200

    Local user with command line parameters and script manual

--- kasutaja_loomine_1.ps1
+++ kasutaja_loomine_1.ps1
@@ -1,7 +1,10 @@
-﻿param(
-    $username,
-    $fullname,
-    $description
-)
-$userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
-New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
+﻿if($args.Count -ne 3) {
+    Write-Host -ForegroundColor Red '.\script username "Full Name" "Account description"'
+} else {
+    $username = $args[0]
+    $fullname = $args[1]
+    $description = $args[2]
+    
+    $userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+    New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
+}
