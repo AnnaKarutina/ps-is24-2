1b323dc24164bd0649b4db2e3700e8922c018ca7
Anna Karutina <anna.karutina@khk.ee>
2024-12-02 08:56:02 +0200

    Local user with password via command line parameters

---
+++ kasutaja_loomine_1.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    $username,
+    $fullname,
+    $description
+)
+$userPassword = ConvertTo-SecureString 'qwerty' -AsPlainText -Force
+New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
