25fbfd7413d3686c7fd377626cc63bdfcb703d38
Anna Karutina <anna.karutina@khk.ee>
2024-12-02 09:06:26 +0200

    Local users from file

---
+++ kasutajate_loomine_failist.ps1
@@ -0,0 +1,8 @@
+﻿$users = Import-Csv "C:\Users\Anna\kasutajate_haldus\kasutajad.csv" -Encoding Default -Delimiter ";"
+foreach ($user in $users) {
+    $username = $user.Username
+    $fullname = $user.Fullname
+    $description = $user.Description
+    $password = $user.Password | ConvertTo-SecureString -AsPlainText -Force
+    New-LocalUser $username -Password $userPassword -FullName $fullname -Description $description
+}
